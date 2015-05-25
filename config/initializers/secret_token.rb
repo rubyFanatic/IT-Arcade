fdd5c8e11d8c472fb99b4639eae19e442c817159
Ram Manusani <rmanusani@kixeye.com>
2015-05-25 12:23:42 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Consulting::Application.config.secret_key_base = '73c6e05f7e78b4a6faa22a95b51396b4afe2041fe8882c116539be4711402f1d992bd248efff38ee6e42c695e9652356a7094e7e7c1bb5379d5897f0a38107fe'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Consulting::Application.routes.draw do
+  root "pages#home"
+ get "about" => "pages#about"
+  get "contact" => "pages#contact"
+end
