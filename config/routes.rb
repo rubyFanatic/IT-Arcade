448b958bcf4a68ef07a70f84b9a7afa70ee6150d
Ram Manusani <rmanusani@kixeye.com>
2015-05-31 18:06:22 -0700

    Cloud, Big Data and Enterprise pages added

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Consulting::Application.routes.draw do
   get "about" => "pages#about"
   get "contact" => "pages#contact"
   get "career" => "pages#career"
+  get "cloud_solutions" => "pages#cloud_solutions"
+  get "big_data_solutions" => "pages#big_data_solutions"
+  get "enterprise_portal_solutions" => "pages#enterprise_development_solutions"
   
 end
