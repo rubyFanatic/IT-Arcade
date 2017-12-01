01810dedcd42ebf49d2fa93e9a353439450a8702
rubyfanatic <ram.manusani@gmail.com>
2017-11-30 19:15:13 -0800

    Push for emails

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,11 +1,11 @@
 class MessageMailer < ApplicationMailer
-  
-  default :to => "ram.rubyfanatic@gmail.com"
+
+  default :to => ENV['EMAIL_TO']
 
   def message_me(msg)
     @msg = msg
 
     mail from: @msg.email, subject: @msg.subject, body: @msg.content
-  
+
   end
 end
